a867e431fceeb0a9100cf115b967b9c92f74f85d
JP <juanpabloduette@gmail.com>
2024-07-01 15:25:13 -0300

    modified validatelogin.php to not injections sql

--- validatelogin.php
+++ validatelogin.php
@@ -3,8 +3,51 @@ session_start();
 
 
 if (isset($_POST['usuario']) && !empty($_POST['usuario']) && isset($_POST['password']) && !empty($_POST['password'])) {
-    $usuario = $_POST['usuario'];
-    $password = $_POST['password'];
+
+    function limpiar_cadena($cadena)
+    {
+        $cadena = trim($cadena);
+        $cadena = stripslashes($cadena);
+        $cadena = str_ireplace("<script>", "", $cadena);
+        $cadena = str_ireplace("</script>", "", $cadena);
+        $cadena = str_ireplace("<script src", "", $cadena);
+        $cadena = str_ireplace("<script type=", "", $cadena);
+        $cadena = str_ireplace("SELECT * FROM", "", $cadena);
+        $cadena = str_ireplace("DELETE FROM", "", $cadena);
+        $cadena = str_ireplace("INSERT INTO", "", $cadena);
+        $cadena = str_ireplace("DROP TABLE", "", $cadena);
+        $cadena = str_ireplace("DROP DATABASE", "", $cadena);
+        $cadena = str_ireplace("TRUNCATE TABLE", "", $cadena);
+        $cadena = str_ireplace("SHOW TABLES;", "", $cadena);
+        $cadena = str_ireplace("SHOW DATABASES;", "", $cadena);
+        $cadena = str_ireplace("<?php", "", $cadena);
+        $cadena = str_ireplace("?>", "", $cadena);
+        $cadena = str_ireplace("--", "", $cadena);
+        $cadena = str_ireplace("^", "", $cadena);
+        $cadena = str_ireplace("<", "", $cadena);
+        $cadena = str_ireplace("[", "", $cadena);
+        $cadena = str_ireplace("]", "", $cadena);
+        $cadena = str_ireplace("==", "", $cadena);
+        $cadena = str_ireplace(";", "", $cadena);
+        $cadena = str_ireplace("::", "", $cadena);
+        $cadena = trim($cadena);
+        $cadena = stripslashes($cadena);
+        return $cadena;
+    }
+
+    function sanitizing_filter($string)
+    {
+        $pattern = '/(DROP TABLE|SELECT \* FROM|TRUNCATE TABLE|SHOW TABLES|\<|==|\s|\<?php|\?|\/|--|\:|\;|script|\>|\^|\[|\]|DELETE FROM|INSERT INTO|DROP DATABASE|SHOW DATABASE)/i';
+        $string =  preg_replace($pattern, '', $string);
+        return  $string;
+    }
+
+    // $usuario = $_POST['usuario'];
+    // $password = $_POST['password'];
+    // $usuario = limpiar_cadena($_POST['usuario']);
+    // $password = limpiar_cadena($_POST['password']);
+    $usuario = sanitizing_filter($_POST['usuario']);
+    $password = sanitizing_filter($_POST['password']);
     $_SESSION['usuario'] = $usuario;
     require "conexion.php";
     $consulta = $pdo->prepare("SELECT * FROM usuarios WHERE nom_usuario = :usuario AND password = :password");
@@ -13,6 +56,8 @@ if (isset($_POST['usuario']) && !empty($_POST['usuario']) && isset($_POST['passw
     // $consulta->execute();
 
     $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
+
 
     if (empty($resultado)) {
         echo json_encode(array('success' => 0));
